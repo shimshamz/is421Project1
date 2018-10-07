ab0ce9c3750fddfab22d88a04a618548e1ec4c80
shimshamz <31662802+shimshamz@users.noreply.github.com>
2018-10-06 23:43:51 -0400

    Feature: Create Record object using Factory Pattern

--- public/index.php
+++ public/index.php
@@ -12,3 +12,25 @@ class main  {
 
     }
 }
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'first', $value = 'keith') {
+        $this->{$name} = $value;
+    }
+}
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
+    }
+}
